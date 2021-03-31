b50665e6a6cd904e9d3a53da0d28986be9ec4f27
svensktutby <svensk.tut.by@gmail.com>
2021-03-31 19:54:30 +0300

    Updates

---
+++ precache-manifest.ad82baee234b348c8f7dfb8bc57bb928.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591fc473acf7faf1343e61fd4b98f2ca",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "558813fadd4a01f74d52",
+    "url": "/todolist/static/css/main.84e14521.chunk.css"
+  },
+  {
+    "revision": "ee0b06061573b5e999ff",
+    "url": "/todolist/static/js/2.a43bfb43.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/todolist/static/js/2.a43bfb43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "558813fadd4a01f74d52",
+    "url": "/todolist/static/js/main.19b17f36.chunk.js"
+  },
+  {
+    "revision": "c3d5ccae1ee3a7bf91ff",
+    "url": "/todolist/static/js/runtime-main.16223656.js"
+  }
+]);
